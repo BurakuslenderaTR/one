0f7e475333bb500f655cf92aff52b2884f1ffd96
BurakuslenderaTR <72018039+BurakuslenderaTR@users.noreply.github.com>
2020-11-23 19:32:29 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+    class Program {
+        public static void main(String[] args) {
+            Scanner a =new Scanner(System.in);
+            int ilk , iki , sonuc = 0;
+            System.out.println("İlk sayıyı giriniz.");
+            ilk = a.nextInt();
+            System.out.println("İkinci sayıyı giriniz.");
+            iki = a.nextInt();
+            if(ilk > iki) {sonuc = ilk % iki;
+            }else if (iki < ilk)
+            {sonuc = iki % ilk; }else sonuc = 0;{}
+            if (sonuc == 0) { System.out.println("Birbirine kalansız ve tam bölünür."); }
+            else if (sonuc==0);
+            System.out.println("Birbirine tam bölünemez ve kalan sonuç = "+sonuc+"  olur.");} }
